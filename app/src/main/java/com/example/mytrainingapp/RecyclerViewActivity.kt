2e92d1f117d56881933cbe4d08d78b33f6d188f1
Znth3 <zynthkas@gmail.com>
2021-03-10 12:03:55 +0700

    Grid RecyclerView and menu is functioning

--- app/src/main/java/com/example/mytrainingapp/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/mytrainingapp/RecyclerViewActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -28,6 +29,12 @@ class RecyclerViewActivity<T> : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.recycler_menu, menu)
         return super.onCreateOptionsMenu(menu)
@@ -41,11 +48,11 @@ class RecyclerViewActivity<T> : AppCompatActivity() {
     private fun setMode(itemId: Int) {
         when (itemId) {
             R.id.action_list -> {
-
+                showRecyclerList()
             }
 
             R.id.action_grid -> {
-
+                showRecyclerGrid()
             }
 
             R.id.action_cardView -> {
