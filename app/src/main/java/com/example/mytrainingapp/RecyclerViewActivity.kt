8c71bdbdf9a9f539a06db33b61de407c537b50a8
Znth3 <zynthkas@gmail.com>
2021-03-10 11:49:31 +0700

    List RecyclerView completed with menu for change that not functioning

--- app/src/main/java/com/example/mytrainingapp/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/mytrainingapp/RecyclerViewActivity.kt
@@ -2,6 +2,8 @@ package com.example.mytrainingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -24,5 +26,31 @@ class RecyclerViewActivity<T> : AppCompatActivity() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.recycler_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(itemId: Int) {
+        when (itemId) {
+            R.id.action_list -> {
+
+            }
+
+            R.id.action_grid -> {
+
+            }
+
+            R.id.action_cardView -> {
+
+            }
+        }
     }
 }
